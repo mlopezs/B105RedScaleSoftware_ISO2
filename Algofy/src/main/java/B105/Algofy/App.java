26f70c948fa182cddea63bf30c87463300f4e75e
Marcos <marcoslopsob@gmail.com>
2017-11-07 14:04:34 +0100

    Primer commit

---
+++ Algofy/src/main/java/B105/Algofy/App.java
@@ -0,0 +1,13 @@
+package B105.Algofy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
