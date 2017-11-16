fbd12ae8b58d15b260c315f5c58fad5609a05ccf
Marcos <marcoslopsob@gmail.com>
2017-11-16 19:20:44 +0100

    Primera prueba buena

---
+++ Scalefy_RSS_ISO2/src/main/java/B105/Algofy/App.java
@@ -0,0 +1,18 @@
+package B105.Algofy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("Bye bye world");
+        
+        System.out.println();
+        
+    }
+}
