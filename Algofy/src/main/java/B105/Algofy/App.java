d312343fbe203220e265451ae321797e90646cd6
Marcos <marcoslopsob@gmail.com>
2017-11-16 18:42:57 +0100

    Prueba

--- Algofy/src/main/java/B105/Algofy/App.java
+++ Algofy/src/main/java/B105/Algofy/App.java
@@ -9,6 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        // Prueba
+        
+        System.out.println("Bye bye world");
+        
+        System.out.println();
+        
     }
 }
