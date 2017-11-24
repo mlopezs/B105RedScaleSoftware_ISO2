9e68c808428bd72898989417303e064fb78d2b7b
AlfonsoBarragan <32295133+AlfonsoBarragan@users.noreply.github.com>
2017-11-24 21:52:27 +0100

    Agregados los gestores de canciones y albumes para admin

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion2/src/main/java/dominio/GestorCanciones_Admin.java
@@ -0,0 +1,16 @@
+package dominio;
+
+import persistencia.*;
+
+public class GestorCanciones_Admin {
+
+	/**
+	 * 
+	 * @param cancion
+	 */
+	public int anadirCancion(Cancion cancion) {
+		// TODO - implement GestorCanciones_Admin.a�adirCancion
+		throw new UnsupportedOperationException();
+	}
+
+}
