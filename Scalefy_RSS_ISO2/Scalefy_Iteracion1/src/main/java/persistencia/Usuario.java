bd54d0b7bf895722e47a7a26c66f1d05768fd01b
Marcos <marcoslopsob@gmail.com>
2017-11-24 11:46:24 +0100

    Añadida clase Usuario.

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion1/src/main/java/persistencia/Usuario.java
@@ -0,0 +1,42 @@
+package Persistencia;
+
+public class Usuario {
+
+    private String nombre;
+    private String apellidos;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+        
+}
