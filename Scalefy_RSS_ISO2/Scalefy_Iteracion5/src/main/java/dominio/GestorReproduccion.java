4d9b7284431a7936b31761b3f945498925e3fb8e
Marcos <marcoslopsob@gmail.com>
2017-11-28 22:31:32 +0100

    Comppletada iteración 5 - (Lucia)

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion5/src/main/java/dominio/GestorReproduccion.java
@@ -0,0 +1,26 @@
+package dominio;
+
+/**
+*@version 1.0
+*/
+public class GestorReproduccion {
+
+	/**
+	 * 
+	 * @param idCancion
+	 */
+	public void reproducirCancion(int idCancion) {
+		// TODO - implement GestorReproduccion.reproducirCancion
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idAlbum
+	 */
+	public void reproducirAlbum(int idAlbum) {
+		// TODO - implement GestorReproduccion.reproducirAlbum
+		throw new UnsupportedOperationException();
+	}
+
+}
