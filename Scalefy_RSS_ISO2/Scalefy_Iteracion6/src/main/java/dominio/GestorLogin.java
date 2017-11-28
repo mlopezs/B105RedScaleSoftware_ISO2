0f902af415c312d634d34aa0a04611849261d606
AlfonsoBarragan <32295133+AlfonsoBarragan@users.noreply.github.com>
2017-11-28 22:59:28 +0100

    Agregado el gestor de login y la interfaz del mismo

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion6/src/main/java/dominio/GestorLogin.java
@@ -0,0 +1,18 @@
+package dominio;
+
+/**
+*@version 1.0
+*/
+public class GestorLogin {
+
+	/**
+	 * 
+	 * @param usuario
+	 * @param contraseña
+	 */
+	public boolean autenticar(String usuario, char[] contraseña) {
+		// TODO - implement GestorLogin.autenticar
+		throw new UnsupportedOperationException();
+	}
+
+}
