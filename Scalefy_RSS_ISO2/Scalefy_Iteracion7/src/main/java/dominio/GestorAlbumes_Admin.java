258259a530c022ea8e2b0fa9537fb8e688e23aec
Geadalu <luciapiedrabuena1@gmail.com>
2017-11-29 19:02:35 +0100

    Añadida Iteracion7

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion7/src/main/java/dominio/GestorAlbumes_Admin.java
@@ -0,0 +1,28 @@
+package dominio;
+
+import persistencia.*;
+
+/**
+*@version 1.1
+*/
+public class GestorAlbumes_Admin {
+
+	/**
+	 * 
+	 * @param album
+	 */
+	public int añadirAlbum(Album album) {
+		// TODO - implement GestorAlbumes_Admin.añadirAlbum
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idAlbum
+	 */
+	public boolean eliminarAlbum(int idAlbum) {
+		// TODO - implement GestorAlbumes_Admin.eliminarAlbum
+		throw new UnsupportedOperationException();
+	}
+
+}
