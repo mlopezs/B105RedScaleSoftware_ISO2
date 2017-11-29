7cfe28641e38ab8d988522a1e7ad333640a6c8ef
alvaritonoli7 <32325298+alvaritonoli7@users.noreply.github.com>
2017-11-29 08:50:01 +0100

    Añadido GestorAlbumes_Admin, GestorCanciones_Admin, album, cancion, interfazAdmin, interfazAlbumes_Admin, InterfazCanciones_Admin

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion3/src/main/java/dominio/GestorAlbumes_Admin.java
@@ -0,0 +1,18 @@
+package dominio;
+
+import persistencia.*;
+
+/**
+*@version 1.0
+*/
+public class GestorAlbumes_Admin {
+
+	/**
+	 * 
+	 * @param album
+	 */
+	public int añadirAlbum(Album album) {
+		// TODO - implement GestorAlbumes_Admin.añadirAlbum
+		throw new UnsupportedOperationException();
+	}
+}
