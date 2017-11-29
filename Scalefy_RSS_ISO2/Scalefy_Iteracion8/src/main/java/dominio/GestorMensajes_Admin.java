15b3765643392d765a8f757768b4de9aefedb580
AlfonsoBarragan <32295133+AlfonsoBarragan@users.noreply.github.com>
2017-11-29 18:42:08 +0100

    Agregado el gestor de mensajeria del admin

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion8/src/main/java/dominio/GestorMensajes_Admin.java
@@ -0,0 +1,18 @@
+package dominio;
+
+/**
+* @version 1.0
+*/
+public class GestorMensajes_Admin {
+
+	/**
+	 * 
+	 * @param idUsuario
+	 * @param mensaje
+	 */
+	public boolean enviarMensaje(int idUsuario, String mensaje) {
+		// TODO - implement GestorMensajes_Admin.enviarMensaje
+		throw new UnsupportedOperationException();
+	}
+
+}
