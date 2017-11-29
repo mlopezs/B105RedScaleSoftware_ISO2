c47bc3bf0a2091cc8a5c08ced7958bf4aa4dfb78
alvaritonoli7 <32325298+alvaritonoli7@users.noreply.github.com>
2017-11-29 20:11:13 +0100

    Añadido usuario, interfazAdmin, interfazMensajeria_admin

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion8/src/main/java/persistencia/Usuario.java
@@ -0,0 +1,16 @@
+package persistencia;
+
+import java.util.Queue;
+
+/**
+* @version 1.1
+*/
+public class Usuario {
+
+	private String nombre;
+	private String apellidos;
+	private String nombreUsuario;
+	private String contraseña;
+	private Queue<String> mensajes;
+	
+}
