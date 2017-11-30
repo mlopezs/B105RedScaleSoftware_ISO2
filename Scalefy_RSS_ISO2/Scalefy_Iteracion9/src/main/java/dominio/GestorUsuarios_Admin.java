3617fb0cea078e78ef8dfc798cf88e7d4860ce0c
Marcos <marcoslopsob@gmail.com>
2017-11-30 18:09:20 +0100

    Añadidas clases GestorUsuario_Admin, Usuario.

---
+++ Scalefy_RSS_ISO2/Scalefy_Iteracion9/src/main/java/dominio/GestorUsuarios_Admin.java
@@ -0,0 +1,28 @@
+package dominio;
+
+import persistencia.*;
+
+/**
+* @version 1.1
+*/
+public class GestorUsuarios_Admin {
+
+	/**
+	 * 
+	 * @param usuario
+	 */
+	public int añadirUsuario(Usuario usuario) {
+		// TODO - implement GestorUsuarios_Admin.añadirUsuario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idUsuario
+	 */
+	public boolean eliminarUsuario(int idUsuario) {
+		// TODO - implement GestorUsuarios_Admin.eliminarUsuario
+		throw new UnsupportedOperationException();
+	}
+
+}
