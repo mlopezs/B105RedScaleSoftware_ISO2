ea4c2340bf64e87799aee8d412be84ebec534035
alvaritonoli7 <32325298+alvaritonoli7@users.noreply.github.com>
2017-12-13 17:45:49 +0100

    Implemetado gestorReproducción

--- Scalefy_RSS_ISO2/src/main/java/dominio/GestorReproduccion.java
+++ Scalefy_RSS_ISO2/src/main/java/dominio/GestorReproduccion.java
@@ -1,35 +1,45 @@
 package dominio;
 
+
+
 /**
  * @version 1.1
  */
 public class GestorReproduccion {
-
-   /**
-    * 
-    * @param idCancion 
-    */
+    
+        private double duracion;
+        private int[] numCancionesAlbum;
+        private int[] numCancionesLista;
+        
     public void reproducirCancion(int idCancion) {
-        // TODO - implement GestorReproduccion.reproducirCancion
-        throw new UnsupportedOperationException();
+        
+        if(idCancion!=0){
+             System.out.println("Reproduciendo canción: "+ idCancion+"\n"+"Duracion de la cancion: "+this.duracion);
+        }else{
+            System.out.println("Error al reproducir. La cancion no exite.");
+        }  
     }
 
-    /**
-     *
-     * @param idAlbum
-     */
+    
     public void reproducirAlbum(int idAlbum) {
-        // TODO - implement GestorReproduccion.reproducirAlbum
-        throw new UnsupportedOperationException();
+        
+        if(numCancionesAlbum.length !=0){
+            System.out.println("Reproduciendo album: "+idAlbum);
+        } else {
+            System.out.println("Error al reproducir el album. No existe o está vacio");
+        }
+        
     }
 
-    /**
-     *
-     * @param idListaReproduccion
-     */
+   
     public void reproducirListaReproduccion(int idListaReproduccion) {
-        // TODO - implement GestorReproduccion.reproducirListaReproduccion
-        throw new UnsupportedOperationException();
+        
+        if(numCancionesLista.length !=0){
+            System.out.println("Reproduciendo lista: "+idListaReproduccion);
+        }else{
+            System.out.println("Error al reproducior la lista. No existe o está vacia");
+        }
+       
     }
 
 }
