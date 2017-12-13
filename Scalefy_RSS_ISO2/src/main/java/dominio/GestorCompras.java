40c11a437bd1ea5f3849ea3d208c58c155a0359f
alvaritonoli7 <32325298+alvaritonoli7@users.noreply.github.com>
2017-12-13 18:10:46 +0100

    Implemetado gestorCompra

--- Scalefy_RSS_ISO2/src/main/java/dominio/GestorCompras.java
+++ Scalefy_RSS_ISO2/src/main/java/dominio/GestorCompras.java
@@ -5,24 +5,20 @@ package dominio;
  */
 public class GestorCompras {
 
-    /**
-     * 
-     * @param idCancion
-     * @return 
-     */
+    
     public boolean adquirirCancion(int idCancion) {
-        // TODO - implement GestorCompras.adquirirCancion
-        throw new UnsupportedOperationException();
+        
+        System.out.println("Cancion "+idCancion+" adquirida");
+        return true;
+        
     }
 
-    /**
-     * 
-     * @param idAlbum
-     * @return 
-     */
+   
+    
     public boolean adquirirAlbum(int idAlbum) {
-        // TODO - implement GestorCompras.adquirirAlbum
-        throw new UnsupportedOperationException();
+        
+        System.out.println("Album "+idAlbum+" adquirido");
+        return true;
     }
 
 }
