43c16458e419b9d8c92a6bc4054035deeae2c255
alvaritonoli7 <32325298+alvaritonoli7@users.noreply.github.com>
2017-12-13 18:06:09 +0100

    Implemetado gestorUsuario

--- Scalefy_RSS_ISO2/src/main/java/persistencia/Usuario.java
+++ Scalefy_RSS_ISO2/src/main/java/persistencia/Usuario.java
@@ -20,6 +20,63 @@ public class Usuario {
 		return this.saldo;
 	}
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int[] getCancionesAdquiridas() {
+        return cancionesAdquiridas;
+    }
+
+    public void setCancionesAdquiridas(int[] cancionesAdquiridas) {
+        this.cancionesAdquiridas = cancionesAdquiridas;
+    }
+
+    public int[] getListasReproduccion() {
+        return listasReproduccion;
+    }
+
+    public void setListasReproduccion(int[] listasReproduccion) {
+        this.listasReproduccion = listasReproduccion;
+    }
+
+    public Queue<String> getMensajes() {
+        return mensajes;
+    }
+
+    public void setMensajes(Queue<String> mensajes) {
+        this.mensajes = mensajes;
+    }
+        
+
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
